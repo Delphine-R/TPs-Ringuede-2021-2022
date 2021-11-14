d857ac9a622ee4efc196e6f640809e4718772192
delph <delph@192.168.114.42>
2021-11-14 16:41:13 +0100

    TP2 : exo 3 et l'autre 3 c'est le 4

---
+++ TP2_manip_Ringuede/src/tp2_manip_ringuede/Moussaka.java
@@ -0,0 +1,20 @@
+package tp2_manip_ringuede;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author delph
+ */
+public class Moussaka {
+    
+    int nbCalories;
+    
+    public Moussaka(int unNombre){
+        nbCalories = unNombre;
+    }
+    
+}
---
+++ TP2_manip_Ringuede/src/tp2_manip_ringuede/Tartiflette.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_ringuede;
+
+/**
+ *
+ * @author delph
+ */
+public class Tartiflette {
+    
+    int nbCalories;
+    
+    public Tartiflette(int unNombre){
+        nbCalories = unNombre;
+    }
+    
+}
