eb9b45f5ca719b910f4a13dcb82a1cbddef538de
delph <delph@192.168.114.42>
2021-11-17 12:42:05 +0100

    Projet SP4 Console avancement :  modifs et avancement de la methode débuter partie de partie

--- SP4_console_Ringuede/src/sp4_console_ringuede/Partie.java
+++ SP4_console_Ringuede/src/sp4_console_ringuede/Partie.java
@@ -7,9 +7,9 @@ package sp4_console_ringuede;
 
 import java.util.Scanner;//permet les entrées de l'utilisateur
 
-public class Partie {
+public class Partie {//partie est la classe
     
-    Joueur [] ListeJoueurs = new Joueur [2];
+    Joueur [] ListeJoueurs = new Joueur [2];// attribut ListeJoeur de type : tableau rempli de type Joueur
     Joueur joueurCourant;
     Grille grilleJeu;
 
@@ -20,21 +20,20 @@ public class Partie {
 
     public void initialiserPartie(){
       grilleJeu = new Grille();
-      Jeton jeton1 = new Jeton("Rouge");
-      Jeton jeton2 = new Jeton("Jaune");
-     
+      
       for (int i = 0; i < 21; i++){
+          Jeton jeton1 = new Jeton("Rouge");
+          Jeton jeton2 = new Jeton("Jaune");
           ListeJoueurs[0].ajouterJeton(jeton1);
           ListeJoueurs[1].ajouterJeton(jeton2);
       }
     }
     
-    Scanner sc = new Scanner(System.in);//permet les entrées de l'utilisateur
-    
     public void debuterPartie(){
         
         /*
         
+        - initialise les objets : jetonCourant, joueurCourant ( et joueurCourant = listeJoueur[0] )
         - montrer la grille
         - possibilité de placer un desintegrateur ou non ***
         - choix de col pour placer le jeton
@@ -43,13 +42,44 @@ public class Partie {
         - verif gagnant
         - verif fin de partie avec le nbJetonRestant
         
-        */
+        Scanner sc = new Scanner(System.in);//permet les entrées de l'utilisateur
         
-        grille.afficherGrilleSurConsole();
+        Jeton jetonCourant = new Jeton();
+       
+        
+        grilleJeu.afficherGrilleSurConsole();
         
         System.out.println("C'est à votre tour de placer votre jeton\nEntrez un numéro de colone");
-        int colonne = sc.nextInt;
-        boolean placementDispo = grille.colonneRemplie(colonne);
+        int colonne = sc.nextInt();
+        boolean placementDispo = grilleJeu.colonneRemplie(colonne);
+        while (placementDispo = false) {
+           System.out.println("Erreur : la colonne " + colonne + " est remplie.\nEntrez un autre numéro de colone"); 
+           colonne = sc.nextInt();
+           placementDispo = grilleJeu.colonneRemplie(colonne);
+        }
+        
+        grilleJeu.ajouterJetonDansColonne(jetonCourant, colonne);
+        
+        */
+        
+        boolean partieFinie = false;
+        
+        joueurCourant joueur = new joueurCourant();
+        joueur = joueur.listeJoueur[0];
+        
+        if (grilleJeu.etreRemplie() == true){
+            partieFinie = true;
+        }
+        if (grilleJeu.etreGagnantePourJoueur(listeJoueur) == true){
+            partieFinie = true;
+        }
+            
+        
+        
+        
+        
+        
+        
         
         
         
