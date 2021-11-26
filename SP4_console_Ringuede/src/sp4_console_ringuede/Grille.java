2859552924d20acaad95b646f820085aee11a0a3
delph <delph@192.168.114.42>
2021-11-26 18:10:11 +0100

    Projet SP4 Console avancement : pb actuel:  la partie ne se termine pas

--- SP4_console_Ringuede/src/sp4_console_ringuede/Grille.java
+++ SP4_console_Ringuede/src/sp4_console_ringuede/Grille.java
@@ -1,27 +1,20 @@
-    package sp4_console_ringuede;
+package sp4_console_ringuede;
 
 /**
  *
  * @author delph
  */
-
 public class Grille {
 
-   
+    Cellule[][] CellulesJeu;
 
-    Cellule [][] CellulesJeu;
+    public Grille() {
 
-       
+        CellulesJeu = new Cellule[6][7];
 
-    public Grille(){
+        for (int i = 0; i < 6; i++) {
 
-        CellulesJeu = new Cellule [6][7];
-
-       
-
-        for (int i = 0 ; i < 6 ; i++){
-
-            for (int j = 0 ; j < 7 ; j++){
+            for (int j = 0; j < 7; j++) {
 
                 CellulesJeu[i][j] = new Cellule();
 
@@ -30,59 +23,48 @@ public class Grille {
         }
 
     }
-    
 
-    public boolean ajouterJetonDansColonne(Jeton m, int c){
+    public boolean ajouterJetonDansColonne(Jeton m, int c) {
 
         int colonne = c - 1;
 
         Jeton j = m;
 
-       
+        for (int i = 5; i >= 0; i -= 1) {
 
-        for (int i = 5; i >= 0; i-=1){
-
-            if (i == 0 && CellulesJeu [i][colonne].jetonCourant != null){
+            if (i == 0 && CellulesJeu[i][colonne].jetonCourant != null) {
 
                 return false;
 
-               
+            } else if (CellulesJeu[i][colonne].jetonCourant == null) {
 
-            } else if (CellulesJeu [i][colonne].jetonCourant == null){
-
-                CellulesJeu [i][colonne].affecterJeton(j);
+                CellulesJeu[i][colonne].affecterJeton(j);
 
                 return true;
 
-               
-
-            }else{
+            } else {
 
                 continue;
 
             }
 
-           
-
         }
 
-       return false;
+        return false;
 
     }
 
-   
+    public boolean etreRemplie() {
 
-    public boolean etreRemplie(){
+        for (int i = 0; i < 6; i++) {
 
-        for (int i = 0; i < 6; i++){
+            for (int j = 0; j < 7; j++) {
 
-            for (int j = 0; j < 7; j++){
-
-                if (CellulesJeu [i][j].jetonCourant != null){
+                if (CellulesJeu[i][j].jetonCourant != null) {
 
                     continue;
 
-                }else{
+                } else {
 
                     return false; // Ca finit directement la methode? oui
 
@@ -96,15 +78,13 @@ public class Grille {
 
     }
 
-   
+    public void viderGrille() {
 
-    public void viderGrille(){
+        for (int i = 0; i < 6; i++) {
 
-        for (int i = 0; i < 6; i++){
+            for (int j = 0; j < 7; j++) {
 
-            for (int j = 0; j < 7; j++){
-
-                CellulesJeu [i][j].jetonCourant = null;
+                CellulesJeu[i][j].jetonCourant = null;
 
             }
 
@@ -112,330 +92,233 @@ public class Grille {
 
     }
 
-  
+    public void afficherGrilleSurConsole() {
 
-    public void afficherGrilleSurConsole(){
-
-       
-
-        for (int i = 0; i < 6; i++){
+        for (int i = 0; i < 6; i++) {
 
             System.out.println("");
 
-            for (int j = 0; j < 7; j++){
+            for (int j = 0; j < 7; j++) {
 
-                if(CellulesJeu [i][j].jetonCourant == null){
+                if (CellulesJeu[i][j].jetonCourant == null) {
 
                     System.out.print("x ");
 
-                }else if (CellulesJeu [i][j].jetonCourant.lireCouleur() == "Rouge" ){
+                } else if (CellulesJeu[i][j].jetonCourant.lireCouleur() == "Rouge") {
 
                     System.out.print("R ");
 
-                }else{
+                } else {
 
                     System.out.print("J ");
 
                 }
 
-               
-
             }
 
         }
 
     }
 
-
-
-    public boolean celluleOccupee(int ligne, int col){
+    public boolean celluleOccupee(int ligne, int col) {
 
         int l = ligne - 1;
-
         int c = col - 1;
 
-       
-
-        if (CellulesJeu[ligne][col].jetonCourant == null){
+        if (CellulesJeu[ligne][col].jetonCourant == null) {
 
             return false;
 
-        }
-
-        else{
+        } else {
 
             return true;
 
         }
 
-   
+    }
+
+    public String lireCouleurDuJeton(int ligne, int col) {
+
+        return CellulesJeu[ligne][col].lireCouleurDuJeton();
 
     }
 
-   
-
-    public String lireCouleurDuJeton(int ligne , int col){
-
-        CellulesJeu[ligne][col].lireCouleurDuJeton();
-
-        return "";
-
-    }
-
-   
-
-    public boolean etreGagnantePourJoueur(Joueur unJoueur){
-
-       
+    public boolean etreGagnantePourJoueur(Joueur unJoueur) {
 
         String uneCouleur = unJoueur.Couleur; //lit la couleur à chercher
 
-       
-
         //test ligne gagnante
+        for (int c = 0; c < 4; c++) {
 
-        for (int c=0; c<4; c++){
+            for (int l = 0; l < 6; l++) { //couvre les cases de départ
 
-            for (int l=0; l<6; l++){ //couvre les cases de départ
+                for (int i = 0; i < 4; i++) { //lecture des 4 cases voisines à la case de départ
 
-                for (int i=0; i<4; i++){ //lecture des 4 cases voisines à la case de départ
-
-                    if (!lireCouleurDuJeton(l,c+i).equals(uneCouleur)){
-
+                    if (!lireCouleurDuJeton(l, c + i).equals(uneCouleur)) { 
+// cas ou la couleur du jeton est diff de la couleur du joueur => on passe à une autre case de départ
                         break;
+                        
+                    } else { 
+//cas ou la couleur de la cellule à coté de la case de départ est la meme que la couleur du joueur
 
-                    }
-
-                    else{
-
-                        if (i==3 && lireCouleurDuJeton(l,c+i).equals(uneCouleur) )//si il a atteint la dernière case
-
+                        if (i == 3 && lireCouleurDuJeton(l, c + i).equals(uneCouleur)){//si il a atteint la dernière case
                             return true;
-
+                        }
                     }
-
                 }
-
             }
-
         }
-
-       
 
         //test colonne gagnante
+        for (int c = 0; c < 7; c++) {
 
-        for (int c=0; c<7; c++){
+            for (int l = 0; l < 4; l++) {
 
-            for(int l=0; l<4; l++){
+                for (int i = 0; i < 4; i++) { //lecture des 4 cases voisines à la case de départ
 
-                for (int i=0; i<4; i++){ //lecture des 4 cases voisines à la case de départ
-
-                    if (!lireCouleurDuJeton(l+i,c).equals(uneCouleur)){
+                    if (!lireCouleurDuJeton(l + i, c).equals(uneCouleur)) {
 
                         break;
 
-                    }
+                    } else {
 
-                    else{
-
-                        if (i==3 && lireCouleurDuJeton(l+i,c).equals(uneCouleur) )//si il a atteint la dernière case
-
+                        if (i == 3 && lireCouleurDuJeton(l + i, c).equals(uneCouleur))//si il a atteint la dernière case
+                        {
                             return true;
+                        }
 
                     }
 
                 }
 
-               
-
             }
 
         }
-
-       
 
         //test diagonale haute/droite gagnante
+        for (int c = 0; c < 4; c++) {
 
-        for (int c=0; c<4; c++){
+            for (int l = 0; l < 3; l++) {
 
-            for(int l=0; l<3; l++){
+                for (int i = 0; i < 4; i++) { //lecture des 4 cases voisines à la case de départ
 
-                for (int i=0; i<4; i++){ //lecture des 4 cases voisines à la case de départ
-
-                    if (!lireCouleurDuJeton(l+i,c+i).equals(uneCouleur)){
+                    if (!lireCouleurDuJeton(l + i, c + i).equals(uneCouleur)) {
 
                         break;
 
-                    }
+                    } else {
 
-                    else{
-
-                        if (i==3 && lireCouleurDuJeton(l+i,c+i).equals(uneCouleur) )//si il a atteint la dernière case
-
+                        if (i == 3 && lireCouleurDuJeton(l + i, c + i).equals(uneCouleur))//si il a atteint la dernière case
+                        {
                             return true;
+                        }
 
                     }
 
-                }  
+                }
 
             }
 
         }
-
-       
 
         //test diagonale basse/droite gagnante
+        for (int c = 0; c < 4; c++) {
 
-        for (int c=0; c<4; c++){
+            for (int l = 3; l < 6; l++) {
 
-            for(int l=3; l<6; l++){
+                for (int i = 0; i < 4; i++) { //lecture des 4 cases voisines à la case de départ
 
-                for (int i=0; i<4; i++){ //lecture des 4 cases voisines à la case de départ
-
-                    if (!lireCouleurDuJeton(l-i,c+i).equals(uneCouleur)){
+                    if (!lireCouleurDuJeton(l - i, c + i).equals(uneCouleur)) {
 
                         break;
 
-                    }
+                    } else {
 
-                    else{
-
-                        if (i==3 && lireCouleurDuJeton(l-i,c+i).equals(uneCouleur) )//si il a atteint la dernière case
-
+                        if (i == 3 && lireCouleurDuJeton(l - i, c + i).equals(uneCouleur))//si il a atteint la dernière case
+                        {
                             return true;
-
+                        }
                     }
-
-                }  
-
+                }
             }
-
         }
 
-       
-
         return false; // non gagnant
-
     }
 
- 
-
-    public boolean colonneRemplie(int colonne){
-
+    public boolean colonneRemplie(int colonne) {
         int c = colonne - 1;
-
-        for (int i =0; i < 6; i++){
-
-            if (CellulesJeu [i][c].jetonCourant == null){
-
+        for (int i = 0; i < 6; i++) {
+            if (CellulesJeu[i][c].jetonCourant == null) {
                 return false;
-
-            }  
-
+            }
         }
 
         return true;
-
-       
-
     }
 
-   
-
-    public void tasserGrille(int colonne){
+    public void tasserGrille(int colonne) {
 
         int c = colonne - 1;
 
-       
+        for (int i = 0; i < 5; i++) {
 
-        for (int i = 0; i <5; i++){
-
-            CellulesJeu [i][c].jetonCourant = CellulesJeu [i+1][c].jetonCourant;
-
-           
+            CellulesJeu[i][c].jetonCourant = CellulesJeu[i + 1][c].jetonCourant;
 
         }
 
-        CellulesJeu [5][c].jetonCourant = null;
-
-
-    }
-    
-    public boolean placerTrouNoir(int ligne , int col){
-        int l = ligne-1;
-        int c = col-1;
-        return CellulesJeu [l][c].placerTrouNoir();
+        CellulesJeu[5][c].jetonCourant = null;
     }
 
-   
-
-    public boolean placerDesintegrateur (int l, int c){
-
-        int l1 = l-1;
-
-        int c1 = c-1;
-
-       
-
-        return CellulesJeu [l1][c1].placerDesintegrateur();
-  
-
+    public boolean placerTrouNoir(int ligne, int col) {
+        int l = ligne - 1;
+        int c = col - 1;
+        return CellulesJeu[l][c].placerTrouNoir();
     }
 
-   
+    public boolean placerDesintegrateur(int l, int c) {
+        int l1 = l - 1;
+        int c1 = c - 1;
+        return CellulesJeu[l1][c1].placerDesintegrateur();
+    }
 
-    public boolean supprimerJeton(int l, int c){
+    public boolean supprimerJeton(int l, int c) {
 
-        int l2 = l-1;
+        int l2 = l - 1;
 
-        int c2 = c-1;
+        int c2 = c - 1;
 
-       
-
-        if (CellulesJeu[l2][c2].jetonCourant == null){
+        if (CellulesJeu[l2][c2].jetonCourant == null) {
 
             System.out.println("Jeton absent");
 
             return false;
 
-        }
-
-        else{
+        } else {
 
             CellulesJeu[l2][c2].jetonCourant = null;
 
             return true;
 
-           
-
         }
-
-          
 
     }
 
-       
+    public Jeton recupererJeton(int l, int c) {
 
-    public Jeton recupererJeton(int l, int c){
+        int l3 = l - 1;
 
-        int l3 = l-1;
-
-        int c3 = c-1;
+        int c3 = c - 1;
 
         Jeton reference = CellulesJeu[l3][c3].recupererJeton();
 
-       
-
         CellulesJeu[l3][c3].jetonCourant = null;
-
-       
 
         return reference;
 
     }
- 
 
-    }
+}
     
 /*
 public class Grille { //FINI
