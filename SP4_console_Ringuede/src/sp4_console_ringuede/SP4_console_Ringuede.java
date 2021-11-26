4973358d9424f9e5f1cd677ebd74144f000d68a5
delph <delph@192.168.114.42>
2021-11-26 17:26:02 +0100

    Projet SP4 Console avancement : pb actuel:  la partie ne se termine pas

--- SP4_console_Ringuede/src/sp4_console_ringuede/SP4_console_Ringuede.java
+++ SP4_console_Ringuede/src/sp4_console_ringuede/SP4_console_Ringuede.java
@@ -1,7 +1,5 @@
 package sp4_console_ringuede;
 
-import java.util.Scanner;
-
 /**
  *
  * @author delph
@@ -9,12 +7,28 @@ import java.util.Scanner;
 public class SP4_console_Ringuede {
 
     public static void main(String[] args) {
-        System.out.println("Bienvenue dans le Super Puissance 4\nEntrez le nom du Joueur 1: ");
-        Joueur(sc.nextString());
-        
-        
-        
-        
+
+        Partie partie = new Partie();
+        partie.initialiserPartie();
+        partie.debuterPartie();
+
     }
-    
 }
+
+/*
+public class SP4_console_Ringuede {
+
+    public static void main(String[] args){
+
+        Joueur joueur1 = new Joueur("joueur1");
+        Joueur joueur2 = new Joueur("joueur2");
+
+
+        Partie partie = new Partie(joueur1, joueur2);
+
+        partie.initialiserPartie();
+        partie.debuterPartie();
+
+    }
+}
+ */
