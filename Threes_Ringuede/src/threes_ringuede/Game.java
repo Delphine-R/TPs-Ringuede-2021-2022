6e4f2268059692cb90a14286f1f0159ee5767b41
delph <delph@192.168.114.42>
2022-01-02 23:34:40 +0100

    Mini Projet : jeu Threes avancement : il reste des rares bugs au niveau de la fusion mais le reste ça passe.

--- Threes_Ringuede/src/threes_ringuede/Game.java
+++ Threes_Ringuede/src/threes_ringuede/Game.java
@@ -1,7 +1,65 @@
-
 package threes_ringuede;
 
+import java.util.Scanner;
 
 public class Game {
-    
+
+    Grid grid;
+
+    public Player initGame() {
+
+        grid = new Grid();
+        grid.initGrid();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Bienvenue dans le jeu Threes");
+
+        System.out.println("Quel est votre nom de joueur : ");
+        Player player = new Player(sc.nextLine());
+        return player;
+    }
+
+    public boolean round(Player player) {
+        
+        boolean gameOver = false;
+        
+        if (!grid.gridFull()) {
+            grid.newInt();
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("""
+                               
+                               Entrez le numéro correspondant au sens du déplacement : 
+                               1 pour déplacer vers la droite, 
+                               2 pour la gauche, 
+                               3 pour le haut 
+                               4 pour le bas.
+
+                               Votre choix : """);
+            int dir = sc.nextInt();
+            while (dir != 1 && dir != 2 && dir != 3 && dir != 4) {
+                System.out.println("""
+                                   Erreur de saisie
+                                    entrez a nouveau votre choix : """);
+                dir = sc.nextInt();
+            }
+            String d = "aucune direction attribuée pour le moment";
+            switch (dir) {
+                case 1 -> d = "right";
+                case 2 -> d = "left";
+                case 3 -> d = "up";
+                case 4 -> d = "down";
+            }
+            System.out.println(d);
+            grid.move(d);
+
+            player.score++; // chaque tour incrémente son score
+            grid.printGrid(player);
+        } 
+        if (grid.gridFull()) {
+            gameOver = true;
+            System.out.println("La partie est terminée! \nVous avez joué " + player.score + " tours");
+        }
+        return gameOver;
+    }
 }
